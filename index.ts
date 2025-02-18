4096512cf7d6ce811e3888541b1c11692c1bc6b2
obed landu <149078000+ObedLandu@users.noreply.github.com>
2025-02-18 19:45:27 +0100

    second exercice

--- index.ts
+++ index.ts
@@ -4,3 +4,8 @@ function add(a: number, b: number): number {
 
 const result = add(5, 3);
 console.log(result); 
+
+const estPair = (n : number) : boolean => {
+    return n % 2 === 0;
+}
+console.log(estPair(2));
